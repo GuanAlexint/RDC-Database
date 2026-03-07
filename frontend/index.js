a7e8400e19785f3881f9d87dee5ce00d5694f497
GuanAlexint <153444714+GuanAlexint@users.noreply.github.com>
2026-03-07 14:56:05 +0800

    Add files via upload

---
+++ frontend/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import SearchPage from '../views/SearchPage.vue'
+import DetailPage from '../views/DetailPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: SearchPage
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: DetailPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
+
